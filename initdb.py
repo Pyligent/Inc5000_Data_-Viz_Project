f752e920e81ae62917bdb8f5b9180d7817ba7438
TJin <jin.hybr@gmail.com>
2019-03-07 14:47:32 -0500

    reb

--- initdb.py
+++ initdb.py
@@ -8,11 +8,12 @@ from sqlalchemy import create_engine
 
 #URL = "postgres://wwbdqcdgscpyca:e1da1c264f89d9a7a5f1a6803284d7187b128dd95c917d8f53bd09bdebf6c0e0@ec2-54-197-232-203.compute-1.amazonaws.com:5432/d3hjbfss6at681"
 #engine = create_engine(URL)
-#engine = create_engine('postgresql+psycopg2://postgres:postgres@@127.0.0.1/inc5000')
+engine = create_engine('postgresql+psycopg2://wwbdqcdgscpyca:e1da1c264f89d9a7a5f1a6803284d7187b128dd95c917d8f53bd09bdebf6c0e0@ec2-54-197-232-203.compute-1.amazonaws.com:5432/d3hjbfss6at681/inc5000')
 
-DATABASE_URL = os.environ['DATABASE_URL']
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+#DATABASE_URL = os.environ['DATABASE_URL']
+
+#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # 1. Load INC5000 2018 Data into the PostgreSQL Database
 
@@ -24,7 +25,7 @@ df.columns = ['id', 'url', 'rank', 'city', 'ifmid', 'ifiid',
        'longitude', 'yrs_on_list', 'previous_workers', 'metro',
        'partner_lists']
 
-df.to_sql('inc2018_data',  con=conn)
+df.to_sql('inc2018_data',  con=engine)
 
 #2. Load INC5000 2007-2017 Data into the PostgreSQL Database
 
@@ -34,5 +35,5 @@ inc_ten_df.columns = ['year', 'rank', 'city', 'growth', 'workers',
        'company', 'state_s', 'state_l', 'revenue',
        'yrs_on_list', 'industry', 'metro']
 
-inc_ten_df.to_sql('inc2007_2017_data',  con=conn)
+inc_ten_df.to_sql('inc2007_2017_data',  con=engine)
 
